91cfb9c2d55183556380a0d06529fdc91c2c3643
dbustillo11 <117930537+dbustillo11@users.noreply.github.com>
2023-09-03 08:52:23 -0300

    Color light

--- index.js
+++ index.js
@@ -13,19 +13,37 @@ function getCharacters(done) {
 
 getCharacters(data => {
     data.results.forEach(personaje => {
+        let statusClass;
+        switch (personaje.status) {
+            case "Dead":
+                statusClass = "dead";
+                break;
+            case "Alive":
+                statusClass = "alive";
+                break;
+            default:
+                statusClass = "unknown";
+                break;
+        }
+    
         const article = document.createRange().createContextualFragment(/*html*/`
         <article>
             <div class="image-container">
                 <img src="${personaje.image}" alt="Personaje">
             </div>
+            <div class="info">
 
-            <h2>${personaje.name}</h2>
-            <span>${personaje.status}</span>
+                <h2>${personaje.name}</h2>
+                <span class="status-circle ${statusClass}"></span> 
+                <span>${personaje.status}</span>
+            </div>
         </article>
         `);
-
+    
         const main = document.querySelector("main");
-
+    
         main.append(article);
     });
+    
+    
 });
